4c43090c4b65578edb77e221b933d9064fa8270c
Somu-ed <satpathysoumyajeet.99@gmail.com>
2021-01-12 23:08:43 +0530

    time conversion min to sec

--- addNumbers.js
+++
@@ -1,9 +0,0 @@
-console.log('Hello');
-
-function addNumbers(num1, num2) {
-    return num1+num2;
-}
-
-console.log(addNumbers(2, 5));
-console.log(addNumbers(5, 5));
-console.log(addNumbers(15, 5));
---
+++ script.js
@@ -0,0 +1,15 @@
+function addNumbers(num1, num2) {
+    return num1+num2;
+}
+
+// console.log(addNumbers(2, 5));
+// console.log(addNumbers(5, 5));
+// console.log(addNumbers(15, 5));
+
+function convertMinutesToSeconds(min) {
+    return min*60;
+}
+
+console.log(convertMinutesToSeconds(1));
+console.log(convertMinutesToSeconds(5));
+console.log(convertMinutesToSeconds(3));
